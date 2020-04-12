6a809977fc9d6534766ff6e71a1e93fef7eeb974
Hanna Pereverzieva <hporand@gmail.com>
2020-04-12 19:57:14 +0300

    finish timer project

--- Timer/index.js
+++ Timer/index.js
@@ -1,35 +1,25 @@
-class Timer {
-  constructor(durationInput, startButton, pauseButton) {
-    this.durationInput = durationInput;
-    this.startButton = startButton;
-    this.pauseButton = pauseButton;
-
-    this.startButton.addEventListener('click', this.start);
-    this.pauseButton.addEventListener('click', this.pause);
-  }
-
-  start = () => {
-    this.tick();
-    this.interval = setInterval(this.tick, 1000);
-  };
-
-  pause = () => {
-    clearInterval(this.interval);
-  }
-
-  tick = () =>{
-    const timeRemaining = parseFloat(this.durationInput.value);
-    this.durationInput.value = timeRemaining -1;
-  };
-
- 
-}
-
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+const circle = document.querySelector('circle');
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', perimeter);
+let duration;
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  onStart(totalDuration) {
+    duration = totalDuration;
+  },
+  onTick(timeRemaining) {
+
+    circle.setAttribute('stroke-dashoffset',
+     (perimeter * timeRemaining) / duration - perimeter);
+    
+  },
+  onComplete() {
+    console.log('Timer ticked to the end');
+  }
+});
 
 
 

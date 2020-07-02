0112144427657ffe1544779650b9d7288f0420e6
Hanna Pereverzieva <41108545+annyrandie@users.noreply.github.com>
2020-07-02 19:59:16 +0300

    Update index.ts

--- typescript/maps/src/index.ts
+++ typescript/maps/src/index.ts
@@ -1,6 +1,9 @@
 import { User } from './User';
 import { Company } from './Company';
+import { CustomMap } from './CustomMap';
 
+const customMap = new CustomMap('map');
 const user = new User();
+customMap.addMarker(user);
 const company = new Company();
-console.log(user);
+customMap.addMarker(company);

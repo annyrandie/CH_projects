9dc5ba35fb321c7c0440542257b26f17c0d5649a
Hanna Pereverzieva <hporand@gmail.com>
2020-07-07 14:03:47 +0300

    sorted numbers

--- typescript/sort/src/index.ts
+++ typescript/sort/src/index.ts
@@ -1,7 +1,8 @@
-console.log(1234);
+import { Sorter } from './Sorter';
+import { NumbersCollection } from './NumbersCollection';
 
-const logSomething = () => {
-  console.log('something');
-};
+const numbersCollection = new NumbersCollection([-3, 40,8, 0, -10, 26, 100]);
+const sorter = new Sorter(numbersCollection);
+sorter.sort();
 
-logSomething();
+console.log(numbersCollection.data);

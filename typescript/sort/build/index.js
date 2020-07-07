ba5cc4a3a5b9882c9d4dcf043acd27b41ae63b4b
Hanna Pereverzieva <hporand@gmail.com>
2020-07-07 10:12:39 +0300

    added sort project

---
+++ typescript/sort/build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log(1234);
+var logSomething = function () {
+    console.log('something');
+};
+logSomething();
---
+++ typescript/sort/src/index.ts
@@ -0,0 +1,7 @@
+console.log(1234);
+
+const logSomething = () => {
+  console.log('something');
+};
+
+logSomething();

428f1cbdf5ac703d5b161313dd788ee8e54af718
Hanna Pereverzieva <41108545+annyrandie@users.noreply.github.com>
2020-07-08 22:47:34 +0300

    Add files via upload

---
+++ typescript/stats/src/MatchResult.ts
@@ -0,0 +1,8 @@
+import { homedir } from 'os';
+
+//enum - enumeration
+export enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
--- typescript/stats/src/index.ts
+++ typescript/stats/src/index.ts
@@ -1,16 +1,25 @@
 //import file system from node.js
 //to use it install type definition file
 //npm install @types/node
-import fs from 'fs';
-import { rosybrown } from 'color-name';
+// import fs from 'fs';
+import { CsvFileReader } from './CsvFileReader';
+import { MatchResult } from './MatchResult';
 
-const matches = fs
-  .readFileSync('football.csv', {
-    encoding: 'utf-8'
-  })
-  .split('\n')
-  .map((row: string): string[] => {
-    return row.split(',');
-  });
+const reader = new CsvFileReader('football.csv');
 
-console.log(matches);
+reader.read();
+const dateOfFirstMatch = reader.data[0];
+console.log(reader.data[0]);
+//enum - enumeration
+
+let manUnitedWins = 0;
+
+for (let match of reader.data) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man Umited' && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Manchester United won ${manUnitedWins} games`);
